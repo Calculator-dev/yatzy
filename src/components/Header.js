825aeb98738814fa8c18691ff5f43d968bdaea5a
Haris Aksalic <haris_aksalic@hotmail.com>
2021-09-27 14:17:46 +0200

    high score changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,15 @@ import logo from "../style/dice.png"
 
 export default function Header({ highScore }) {
 
-
-
     return (
         <div style={{ width: "100%", backgroundColor: "lightblue", display: "flex", flexDirection: "row", padding: "10px" }} >
             <img src={logo} alt="logo" style={{ width: "30px", height: "30px" }} />
             <Typography variant="button" display="block" style={{ marginLeft: "10px", fontSize: "20px" }} >
                 Yatzy Game
             </Typography>
-            <Typography style={{ position: "absolute", right: "10px", justifyContent: "center", alignItems: "center" }} >
+            {highScore > 0 && <Typography style={{ position: "absolute", right: "10px", justifyContent: "center", alignItems: "center" }} >
                 High Score: {Math.max(...highScore, 0)}
-            </Typography>
+            </Typography>}
         </div >
     )
 }
